7af2fa43f3a7b47659a13f8236345a8d67b8e808
Michael Allan <Michael Allan>
2018-07-15 22:44:11 +1000

    Add About page

---
+++ src/pages/services.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const ServicesPage = () => {
+  return (
+    <div>
+      <h1>Our Services</h1>
+      <p>
+        Doctrina legam an proident exquisitaque, singulis enim ingeniis, malis
+        et mentitum, iudicem si illum appellat id dolore e singulis, pariatur eu
+        probant ad possumus sunt probant, irure incididunt do commodo. Probant
+        reprehenderit est proident o aut quorum quamquam. Officia export tamen
+        occaecat noster. De dolor quis ad consequat e iis quid exercitation, ad
+        ipsum dolore te occaecat et esse consequat ne quamquam.Sint hic iis anim
+        ingeniis te laboris o culpa occaecat, do ea multos ipsum quid, nam
+        singulis sed singulis, eu export reprehenderit, aut occaecat aut
+        litteris, fugiat laboris deserunt ab quo quid admodum. Nisi aliquip
+        litteris. Mentitum arbitrantur in probant, duis non mentitum, nisi
+        deserunt ex familiaritatem hic an quorum senserit arbitrantur quo ne
+        ipsum sint sed possumus ea multos distinguantur nescius multos
+        incurreret, consequat se culpa si quem te nostrud.
+      </p>
+      <p>
+        Summis mentitum sed litteris. Do deserunt ex constias id senserit
+        concursionibus ut officia, elit possumus mandaremus ex ubi aut multos
+        cernantur, ingeniis summis offendit incurreret, aut ex exquisitaque,
+        doctrina nulla constias, o aliquip id excepteur. Tamen vidisse et
+        distinguantur, do tamen hic eram non ab ubi fugiat constias se velit eu
+        iis quorum proident, ubi ita veniam magna eram ubi se cupidatat si
+        incididunt, quae incurreret do multos elit an excepteur qui esse
+        cernantur. Est nam elit fore legam. Irure do appellat si quis ex ullamco
+        sed elit, eu mentitum instituendarum an export pariatur ne malis quem. E
+        de philosophari. Ea nam cillum cillum labore, proident illum arbitror,
+        mandaremus a vidisse id vidisse a laborum.
+      </p>
+    </div>
+  )
+}
+
+export default ServicesPage
