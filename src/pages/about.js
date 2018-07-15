7321924b74b296d95464d4ee1746f890bac8c7c3
Michael Allan <Michael Allan>
2018-07-15 22:35:45 +1000

    Add About page

---
+++ src/pages/about.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>
+        Doctrina legam an proident exquisitaque, singulis enim ingeniis, malis
+        et mentitum, iudicem si illum appellat id dolore e singulis, pariatur eu
+        probant ad possumus sunt probant, irure incididunt do commodo. Probant
+        reprehenderit est proident o aut quorum quamquam. Officia export tamen
+        occaecat noster. De dolor quis ad consequat e iis quid exercitation, ad
+        ipsum dolore te occaecat et esse consequat ne quamquam.Sint hic iis anim
+        ingeniis te laboris o culpa occaecat, do ea multos ipsum quid, nam
+        singulis sed singulis, eu export reprehenderit, aut occaecat aut
+        litteris, fugiat laboris deserunt ab quo quid admodum. Nisi aliquip
+        litteris. Mentitum arbitrantur in probant, duis non mentitum, nisi
+        deserunt ex familiaritatem hic an quorum senserit arbitrantur quo ne
+        ipsum sint sed possumus ea multos distinguantur nescius multos
+        incurreret, consequat se culpa si quem te nostrud.
+      </p>
+    </div>
+  )
+}
+
+export default AboutPage
